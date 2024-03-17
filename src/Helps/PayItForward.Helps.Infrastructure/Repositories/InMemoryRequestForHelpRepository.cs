29a22cc8664291b891c1e42358bc171cfc8ef1e6
Paweł Suliga <pawel.suliga.dev@gmail.com>
2024-03-17 11:41:59 +0100

    feat: Add in memory request for help repository

---
+++ src/Helps/PayItForward.Helps.Infrastructure/Repositories/InMemoryRequestForHelpRepository.cs
@@ -0,0 +1,28 @@
+using PayItForward.Helps.Domain.Aggregates;
+using PayItForward.Helps.Domain.Repositories;
+using PayItForward.Helps.Domain.ValueObjects;
+
+namespace PayItForward.Helps.Infrastructure.Repositories;
+
+public class InMemoryRequestForHelpRepository : IRequestForHelpRepository
+{
+    private static readonly Dictionary<RequestForHelpId, RequestForHelp> RequestsForHelp = new();
+
+    public Task<RequestForHelp> Get(RequestForHelpId id, CancellationToken cancellationToken)
+        => Task.FromResult(RequestsForHelp.GetValueOrDefault(id));
+
+    public Task Create(RequestForHelp requestForHelp, CancellationToken cancellationToken)
+    {
+        RequestsForHelp.Add(requestForHelp.Id, requestForHelp);
+        return Task.CompletedTask;
+    }
+
+    public Task Update(RequestForHelp requestForHelp, CancellationToken cancellationToken)
+        => Task.CompletedTask;
+
+    public Task Delete(RequestForHelpId id, CancellationToken cancellationToken)
+    {
+        RequestsForHelp.Remove(id);
+        return Task.CompletedTask;
+    }
+}
