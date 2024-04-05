190f5f0eb7b9545e44a735777dc9a80b3e938e9d
Paweł Suliga <pawel.suliga.dev@gmail.com>
2024-04-05 21:23:23 +0200

    feat: Consume ActiveHelpWasAbandoned

--- src/Helps/PayItForward.Helps.Application/Events/ActiveHelpWasAbandoned.cs
+++ src/Helps/PayItForward.Helps.Application/Events/ActiveHelpWasAbandoned.cs
@@ -1,3 +1,6 @@
+using PayItForward.Helps.Application.Commands;
+using PayItForward.Helps.Domain.ValueObjects;
+using PayItForward.Shared.CQRS.Commands.Abstractions;
 using PayItForward.Shared.CQRS.Events.Abstractions;
 
 namespace PayItForward.Helps.Application.Events;
@@ -6,5 +9,13 @@ public record ActiveHelpWasAbandoned(Guid ActiveHelpId, Guid AbandoningId) : Int
 
 public class ActiveHelpWasAbandonedHandler : IEventHandler<ActiveHelpWasAbandoned>
 {
-    public Task Handle(ActiveHelpWasAbandoned @event, CancellationToken cancellationToken) => throw new NotImplementedException();
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public ActiveHelpWasAbandonedHandler(ICommandDispatcher commandDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+    }
+
+    public Task Handle(ActiveHelpWasAbandoned @event, CancellationToken cancellationToken)
+        => _commandDispatcher.Execute(new DoNotHelp(new RequestForHelpId(@event.ActiveHelpId), new PotentialHelper(@event.AbandoningId)));
 }
